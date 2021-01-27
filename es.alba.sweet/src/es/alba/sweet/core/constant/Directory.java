b24ee82217c487f7bedd426650f7e58b8f519f9a
malfois <mmalfois@cells.es>
2021-01-27 17:17:37 +0100

    re push

--- es.alba.sweet/src/es/alba/sweet/core/constant/Directory.java
+++ es.alba.sweet/src/es/alba/sweet/core/constant/Directory.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 public class Directory {
 
-	// test 394b44f79fdbba6974f47a520266231670c4c01a
 	public final static String	ICONS	= "icons" + File.separator;
 	public final static String	CONFIG	= "config" + File.separator;
 }
